42cfab131ca564958c408d35a39941b14c16b8b8
vijayk2508 <official.vijay.2508@gmail.com>
2025-01-27 00:11:01 +0530

    Refactor routing to use window.BASEPATH instead of process.env for dynamic base path configuration

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process?.env?.REACT_APP_BASEPATH || "/"}>
+    <BrowserRouter basename={window?.BASEPATH || "/"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
