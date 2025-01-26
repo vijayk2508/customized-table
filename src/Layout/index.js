6fe700b30c2ff1d6dded7edb1c94f27a851529cf
vijayk2508 <official.vijay.2508@gmail.com>
2025-01-27 00:39:32 +0530

    Refactor routing to use window.BASEPATH for dynamic base path configuration

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import React from "react";
 import { useLocation } from "react-router";
 
-const baseURL = window?.BASEPATH?.includes("customized-table")
+const baseURL = window.BASEPATH?.includes("customized-table")
   ? "/customized-table"
-  : "";
+  : "/";
 
 function getURL(url) {
   return baseURL ? baseURL + url : url;
