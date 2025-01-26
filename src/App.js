63ba68be9b0982bb8b32ca55fbfde33521f2e46e
vijayk2508 <vijayk2508@users.noreply.github.com>
2025-01-26 16:51:47 +0000

    deploy: 05b4260ec173e16b264d52241d236df3c2f3e346

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import StaticTabulatorTable from "./Pages/StaticTabulatorTable";
-import GridJs from "./Pages/GridJs";
-// import LandingPage from "./Pages/TabulatorTables/LandingPage";
-// import AddTable from "./Pages/TabulatorTables/AddTable";
-// import Table from "./Pages/TabulatorTables/Table";
+import ReactGrid from "./Pages/ReactGrid";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<StaticTabulatorTable />} />
-      <Route path="/gridjs" element={<GridJs />} />
-        {/* <Route path="/" element={<LandingPage />} />
-        <Route path="/add-table" element={<AddTable />} />
-        <Route path="/table/:id" element={<Table />} /> */}
+      <Route path="/reactgrid" element={<ReactGrid />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/ReactGrid/index.js
+++ src/Pages/ReactGrid/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Layout from "../../Layout";
 
-function GridJs() {
+function ReactGrid() {
   return <Layout>Grid Js</Layout>;
 }
 
-export default GridJs;
+export default ReactGrid;
