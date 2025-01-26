ac0b445f2064a0761be8e89ad8fee8c1c429c57c
vijayk2508 <official.vijay.2508@gmail.com>
2025-01-26 22:36:46 +0530

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders CustomizeTable component', () => {
-  render(<App />);
-  const customizeTableElement = screen.getByTestId('customize-table');
-  expect(customizeTableElement).toBeInTheDocument();
-});
-
